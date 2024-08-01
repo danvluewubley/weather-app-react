425ff9e7d49bcdb11a224479b3cef7f59319f8a1
Daniel Wu <danielw72908@gmail.com>
2024-08-01 17:28:54 -0400

    Set up all the html

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import Search from './Search'
+import Data from "./Data";
+
+function Card() {
+  return (
+    <>
+      <div class="card">
+        <Search />
+        <Data />
+      </div>
+    </>
+  );
+}
+
+export default Card
